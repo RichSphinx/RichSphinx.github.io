57b4985ff29e141910f2fe03e5cc94b2a1358ff2
Mateo Gutierrez Gomez <mateogutigomez@gmail.com>
2024-11-21 12:26:09 -0500

    Adding crypto scripts

---
+++ crypto.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+openssl enc -d -p -aes-256-cbc -pbkdf2 -salt -in htb.tar.gz.enc -out htb.tar.gz -pass file:./$DECRYPT_KEY && rm htb.tar.gz.enc
+
+tar -xvzf htb.tar.gz && rm htb.tar.gz
